1c1566a9656ed4e61767186a654a91356f626fbe
phucbuihuy456-jpg <phucbuihuy456@gmail.com>
2026-06-03 19:26:32 +0700

    project start

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxHealth = 100;
+
+    public float maxHealth
+    {
+        get
+        {
+            return _maxHealth;
+        }
+        set
+        {
+            _maxHealth = value;
+        }
+    }
+
+    private float _currentHealth = 100;
+
+    public float currentHealth
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        set
+        {
+            _currentHealth = value;
+            // Kiểm tra nếu máu giảm xuống 0 hoặc thấp hơn thì gọi hàm Die
+            if (_currentHealth <= 0)
+            {
+                isAlive = false;
+                //Die();
+            }
+        }
+    }
+    private bool _isAlive = true;
+    public bool isAlive
+    {
+        get
+        {
+           return _isAlive;
+        }
+        set
+        {
+           _isAlive = value;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;       // Rigidbody2D của nhân vật
+    public Animator animator;    // Animator cho chạy/idle
+    public SpriteRenderer spriteRenderer; // SpriteRenderer để lật hướng
+    public float moveSpeed = 3.5f;
+    public float jumpForce = 8f;
+
+    private bool isGrounded = true; // Giả lập đơn giản để nhảy
+
+    void Update()
+    {
+        // --- Di chuyển ngang ---
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Di chuyển sang phải
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+            animator.SetBool("isMoving", true);
+            spriteRenderer.flipX = false; // Hướng mặt về phải
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            // Di chuyển sang trái
+            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+            animator.SetBool("isMoving", true);
+            spriteRenderer.flipX = true; // Hướng mặt về trái
+        }
+        else
+        {
+            // Không nhấn gì → đứng yên
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+            animator.SetBool("isMoving", false);
+        }
+
+        // --- Nhảy ---
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Khi chạm bất kỳ collider nào → bật isGrounded
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
